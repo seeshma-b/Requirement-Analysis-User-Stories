134d682e5aba5a0e0c04ff8bcf4c76312c427b97
Tim-Tim-Tim <timgs2002@gmail.com>
2024-11-14 13:05:27 -0500

    Bug fixes with the mysql connection and python interpreter path

--- ITSC3155-Assignments-main/Part1/database.py
+++ ITSC3155-Assignments-main/Part1/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:rootroot@localhost/Part1"
+DATABASE_URL = "mysql+mysqlconnector://root:Timothy_G2002@localhost:3306/itsc3155_group_project_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
